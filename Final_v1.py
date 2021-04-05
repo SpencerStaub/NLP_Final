28275e9fe27035fabcf5bbc5576bb9237f4ae0fc
SpencerStaub <54607079+SpencerStaub@users.noreply.github.com>
2021-04-05 19:49:38 -0400

    Update Final_v1.py
    Added sent, word and entity. 
    Added write to csv.

--- Final_v1.py
+++ Final_v1.py
@@ -1,4 +1,13 @@
 import pandas as pd
+from nltk.corpus import reuters
+import spacy
+import os
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+
+nlp = spacy.load("en_core_web_sm")
+all_stopwords = nlp.Defaults.stop_words
 
 #Loading the text file
 input_file = open("K_Levine_Book_1.txt" , encoding='utf-8').read()
@@ -13,24 +22,43 @@ chapters.pop(0) # Removes the first item in list as this is "
 
 df_chapters = pd.DataFrame()
 
-chapter_n = []
-title = []
+chapter_list = []
+title_list = []
 text_list = []
+sent_list = []
+word_list = []
+entity_list = []
+
+i = 1
 
 for chapter in chapters:
-    i = 1
+
+
     first_line = chapter.split('\n', 1)[0]
     text = chapter[chapter.index('\n')+1:]
-    #text = chapter.split('\n', 1)[1:]
-    chapter_n.append("Chapter " + str(i))
-    title.append(first_line)
-    text_list.append(text)
+
+    analyzed_doc = nlp(text)
+
+    for entity in analyzed_doc.ents:
+        if entity.text.strip() != "":
+
+            chapter_list.append("Chapter " + str(i))
+            title_list.append(first_line)
+            text_list.append(analyzed_doc)
+            sent_list.append(entity.sent.text)
+            word_list.append(entity.text.strip())
+            entity_list.append(entity.label_)
+
     i = i + 1
 
-df_chapters['chapter_number'] = chapter_n
-df_chapters['chapter_title'] = title
+df_chapters['chapter_number'] = chapter_list
+df_chapters['chapter_title'] = title_list
 df_chapters['text'] = text_list
+df_chapters['sentence'] = sent_list
+df_chapters['word'] = word_list
+df_chapters['entitiy'] = entity_list
 
+df_chapters.to_csv('book_1.csv',index=False)
 
 #for i in range(1, len(chapters)+1): #Loops for the number of chapters in the book, starting at chapter 1
     #writeBook = open("{}.txt".format(i), "w+") #Opens a book with the name of i, if it does not exist, it creates one
